e3ac7f52b21acf337d2854a1663ed81f9f8f3a0e
KeryanChelouche <116768956+KeryanChelouche@users.noreply.github.com>
2023-05-22 11:20:58 +0200

    Final version

--- l2r_class.py
+++ l2r_class.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from transformers import BertTokenizer, BertModel
 from torch.utils.data import Dataset, DataLoader
 
@@ -31,7 +32,7 @@ class LTRDataset(Dataset):
             'relevance': torch.tensor(relevance, dtype=torch.float)
         }
 
-
+ 
 class BertLTRModel(nn.Module):
     def __init__(self, bert_model):
         super(BertLTRModel, self).__init__()
@@ -44,3 +45,41 @@ class BertLTRModel(nn.Module):
         relevance_score = self.linear(pooled_output)
         return relevance_score
 
+
+def create_one_hot(target):
+    # Ensure the target is a 2D tensor of shape (batch_size, 1)
+    if len(target.shape) == 1:
+        target = target.view(-1, 1)
+    # Create a one-hot tensor of shape (batch_size, 2)
+    one_hot = torch.zeros(target.size(0), 2).to(target.device)
+    # Fill the one-hot tensor with (score, 1-score) values
+    one_hot[:, 0] = target.squeeze()
+    one_hot[:, 1] = 1 - target.squeeze()
+
+    return one_hot
+
+class WSLSCrossEntropyLoss(nn.Module):
+    def __init__(self, smoothing=0.2):
+        super(WSLSCrossEntropyLoss, self).__init__()
+        self.smoothing = smoothing
+
+    def forward(self, input, target):
+        with torch.no_grad():
+            # We first create a tensor with same shape as input filled with smoothing value
+            true_dist = torch.ones_like(target)
+            # This is to ensure that the non-relevant documents (target != 1) have the (1-target*smoothing, target*smoothing) condition
+            non_relevant_indices = (target != 1).squeeze()
+            relevant_indices = (target == 1).squeeze()
+
+
+            # target = target.view(-1, 1)
+            true_dist[non_relevant_indices] = target[non_relevant_indices].squeeze() * self.smoothing
+
+            # Next, scatter the remaining confidence to the correct class index
+            true_dist[relevant_indices] = 1-self.smoothing
+            true_dist = create_one_hot(true_dist)
+        # We then calculate the log probability of the inputs (this is done internally in CrossEntropyLoss)
+        log_prob = F.log_softmax(input, dim=-1)
+
+        # Our final loss is then the mean negative log likelihood
+        return torch.mean(torch.sum(-true_dist * log_prob, dim=-1))
